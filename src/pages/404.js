cc96bbf51bed028a68efde9e397219c7396a48a1
Max K <maxkuhn@gmail.com>
2022-10-11 12:12:28 -0700

    Add Gatsby stuff

--- src/pages/404.js
+++ src/pages/404.js
@@ -28,7 +28,7 @@ const NotFoundPage = () => {
     <main style={pageStyles}>
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
+        uh oh!!! 😔 we couldn't find what you were looking for uwu
         <br />
         {process.env.NODE_ENV === "development" ? (
           <>
