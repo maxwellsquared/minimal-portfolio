a7c041169a0984febb678ff1f2d9ca3e232acb8d
Max K <maxkuhn@gmail.com>
2022-10-11 15:51:33 -0700

    Minor typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
       <img className="headshot" src="https://avatars.githubusercontent.com/u/2798962?v=4" alt="Smiling person with glasses, short hair, and a beard wearing a suit and a button-up shirt."/>
       <p>Hi! I'm Max Kuhn, a developer and designer.</p>
       <p>I make apps and websites <span aria-hidden="true">💻</span> for people <span aria-hidden="true">👩‍👩‍👦‍👦</span> and particularly intelligent dogs <span aria-hidden="true">🧠🐶</span>.</p>
-      <p>Welcome and bark, bark, woof woof.</p>
+      <p>Welcome and/or bark, bark, woof woof.</p>
     </Layout>
   )
 }
