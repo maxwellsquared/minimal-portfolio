5a3acf66002c4c0af585188112311d209a72abc2
Max K <maxkuhn@gmail.com>
2022-10-11 14:29:32 -0700

    Add image files

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,8 +4,9 @@ import Layout from '../components/layout';
 const Portfolio = () => {
     return (
         <Layout pageTitle="Portfolio">
-            <p>wow! here's some cool websites i made</p>
-
+            <h3>Reportex</h3>
+            <p><i>Reportex was a full-service legal services firm focusing on court reporting. I was the Digital Content Director and the internal lead on our site redesign.</i></p>
+            <p><i>We were bought in 2021 by a big American firm. The next year, I quit to go do programming full-time.</i></p>
         </Layout>
     )
 }
