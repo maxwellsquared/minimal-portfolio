bc415255ffc8563a46709380dbee23d72209b997
Max K <maxkuhn@gmail.com>
2022-10-12 12:59:53 -0700

    Update portfolio wording

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -30,7 +30,7 @@ const Portfolio = () => {
                 <h3><a href="https://www.reportex.ca">Reportex</a></h3>
                 <p><i>Reportex was a full-service legal services firm focusing on court reporting. I was the Digital Content Director and the internal lead on our site redesign. We were bought in 2021 by a big American firm. The next year, I quit to go do programming full-time.</i></p>
                 <StaticImage width="600" alt="Reportex website homepage with decorative image of a piece of sculpture" src="https://github.com/maxwellsquared/minimal-portfolio/blob/main/img/reportex-1.jpg?raw=true"/>
-                <p>We knew the site would need to be heavily edited by non-technical people, so we built it using Wordpress to allow for code-free editing by just about anyone.</p>
+                <p>This was an enormous project that took almost a year to complete! I brought in an external consultant to collaborate on fonts, colours, and branding. We knew the site would need to be heavily edited by non-technical people, so we built it using Wordpress to allow for code-free editing by just about anyone.</p>
                 <StaticImage width="600" alt="Reportex website homepage with decorative image of a piece of sculpture" src="https://github.com/maxwellsquared/minimal-portfolio/blob/main/img/reportex2.jpg?raw=true"/>
                 <p>I ended up having to code quite a bit of functionality from scratch. Some particular favourites were a PHP program to automatically generate and email us filled-in audio request forms for the Supreme Court when a client ordered a transcript and an internal procedures wiki with everything we needed to run our offices.</p>
             </section>
