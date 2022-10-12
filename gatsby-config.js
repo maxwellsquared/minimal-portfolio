ecdc14224eb68d767af8c78d809f65f0a4478fca
Max K <maxkuhn@gmail.com>
2022-10-11 17:38:27 -0700

    Remove unnecessary plugin info

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,6 @@ module.exports = {
     title: `Max Kuhn`,
     siteUrl: `http://www.maxkuhn.ca`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }
+  plugins: ["gatsby-plugin-styled-components",`gatsby-plugin-gatsby-cloud`, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"
 ]
 };
