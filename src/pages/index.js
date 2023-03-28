06148119f36ebc48e594104718d457e744156c07
Max K <maxkuhn@gmail.com>
2023-03-28 13:50:32 -0700

    Update portfolio with passed, add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,29 @@
 import * as React from "react";
-import { Link } from 'gatsby';
-import Layout from '../components/layout'
-
-
+import { Link } from "gatsby";
+import Layout from "../components/layout";
 
 const IndexPage = () => {
-  return (
-    <Layout pageTitle="Max Kuhn">
-      <img className="headshot" src="https://avatars.githubusercontent.com/u/2798962?v=4" alt="Smiling person with glasses, short hair, and a beard wearing a suit and a button-up shirt."/>
-      <p>Hi! I'm Max Kuhn, a developer and designer.</p>
-      <p>I make apps and websites <span aria-hidden="true">💻</span> for people <span aria-hidden="true">👩‍👩‍👦‍👦</span> and particularly intelligent dogs <span aria-hidden="true">🧠🐶</span>.</p>
-      <p>Welcome and/or bark, bark, woof woof.</p>
-    </Layout>
-  )
-}
+    return (
+        <Layout pageTitle="Max Kuhn">
+            <img
+                className="headshot"
+                src="https://avatars.githubusercontent.com/u/2798962?v=4"
+                alt="Smiling person with glasses, short hair, and a beard wearing a suit and a button-up shirt."
+            />
+            <section>
+                <p>Hi! I'm Max Kuhn, a developer and designer.</p>
+                <p>
+                    I make apps and websites <span aria-hidden="true">💻</span>{" "}
+                    for people <span aria-hidden="true">👩‍👩‍👦‍👦</span> and
+                    particularly intelligent dogs{" "}
+                    <span aria-hidden="true">🧠🐶</span>.
+                </p>
+                <p>Welcome and/or bark, bark, woof woof.</p>
+            </section>
+        </Layout>
+    );
+};
 
-export const Head = () => <title>Max Kuhn</title>
+export const Head = () => <title>Max Kuhn</title>;
 
-export default IndexPage
+export default IndexPage;
